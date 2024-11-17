0788b349866f1918537005432461f77ee962ef05
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2024-11-17 02:23:29 -0500

    include processing of images in page

--- include/prepare_input.py
+++ include/prepare_input.py
@@ -1,30 +1,33 @@
 # import libraries
-import pytesseract
-from pypdf import PdfReader
 import os
+import pytesseract
+import io
+from pypdf import PdfReader
 from pdf2image import convert_from_path
 import pandas as pd
 from PIL import Image
-from texify.inference import batch_inference
-from texify.model.model import load_model
-from texify.model.processor import load_processor
-model = load_model()
-processor = load_processor()
 
 
 def read_from_pdf(path):
     '''Reads a PDF file from path and returns as text content.'''
     reader = PdfReader(path)
     text = ""
-    for i in range(len(reader.pages)):
-        p = reader.pages[i]
+    for idx in range(len(reader.pages)):
+        p = reader.pages[idx]
         text += p.extract_text()
+        images = []
+        for img in p.images:
+            image = Image.open(io.BytesIO(img.data))
+            text += read_from_image(None, image)
     return text
 
 
-def read_from_image(path):
+def read_from_image(path=None, image=None):
     '''Reads an image file from path and returns as text content.'''
-    return pytesseract.image_to_string(Image.open(path))
+    if path:
+        return pytesseract.image_to_string(Image.open(path))
+    if image:
+        return pytesseract.image_to_string(image)
 
 
 def read_from_excel(path):
@@ -33,5 +36,5 @@ def read_from_excel(path):
     return df.to_string()
 
 
-# dir = os.path.dirname(os.path.dirname(__file__))
-# print(read_from_pdf(f"{dir}/input/Q4-taylor.pdf"))
+dir = os.path.dirname(os.path.dirname(__file__))
+print(read_from_pdf(f"{dir}/input/pdf_test.pdf"))
